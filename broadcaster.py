9ad9a54a3dc3b30ecc6ca8c46d858018200ec9ab
Cooper Bartlett <cooperbar@gmail.com>
2021-04-11 12:31:23 +1000

    Updates broadcaster

--- broadcaster.py
+++ broadcaster.py
@@ -1,35 +1,41 @@
 import sys
 import json
 import subprocess
-from flask import Flask
+from quart import Quart
 import random
 import asyncio
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 
 @app.route('/')
-def base():
-    return "Broadcaster Flask page"
+async def base():
+    return "Broadcaster Quart page"
 
 
 @app.route('/switch')
-def switch():
+async def switch():
     new_details = get_random_details()
-    loop = asyncio.get_event_loop()
-    loop.call_later(10, perform_switch, new_details)
+    #loop = asyncio.get_event_loop()
+    #loop.call_soon(perform_switch, new_details)
+    # asyncio.run(perform_switch(new_details))
+    task = asyncio.create_task(perform_switch(new_details))
     return new_details
 
 
-def perform_switch(details):
+async def perform_switch(details):
+    print("Performing switch")
+    await asyncio.sleep(10)
+    print("Stopping hotspot")
     stop_status = stop_hotspot()
-    set_status = set_hostpot(details['ssid'], details['password'])
+    print("Hotspot stopped with message: {}".format(stop_status))
+    set_status = set_hotspot(details['ssid'], details['password'])
     if not set_status:
         print("Error. Could not set hospot")
         return
     print(
         f"Set hostpot to {details['ssid']} with password {details['password']}")
-    start_status = start_hostpot()
+    start_status = start_hotspot()
     if not start_status:
         print("Error. Could not start hotspot")
         return
@@ -87,7 +93,12 @@ def stop_hotspot():
 
 if __name__ == '__main__':
     if check_admin_priv():
-        setup_hotspot()
+        if check_supported_drivers():
+            setup_hotspot()
+            app.run(host='0.0.0.0', threaded=False)
+        else:
+            print("You do not have the required drivers to run the hostednetwork")
+            exit()
     else:
         print("Not running with admin privelidges")
         exit()
